8a0d947662a84f59d65f0fc0d8f8e0f13d87d64a
wjdaudrbs456@naver.com <wjdaudrbs456@naver.com>
2023-08-05 11:00:00 +0000

    스크롤뷰 상하좌우 완료
    터치이벤트로 상하좌우 스크롤뷰 적용

--- app/src/main/java/com/test/scrollview/MainActivity.kt
+++ app/src/main/java/com/test/scrollview/MainActivity.kt
@@ -2,32 +2,69 @@ package com.test.scrollview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.HorizontalScrollView
-import android.widget.ListView
-import androidx.recyclerview.widget.RecyclerView
-import com.test.scrollview.databinding.ActivityMainBinding
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
-    private var items = arrayListOf<listData>()
-    lateinit var adapter: Adapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-        adapter = Adapter(items, this)
-        binding.list.adapter = adapter
+        setContentView(R.layout.activity_main)
 
-        for (i in 0..30) {
-            items.add(
-                listData(
-                    ""
-                )
-            )
+
+        val headerView002 = findViewById<LinearLayout>(R.id.testHeaderView002)
+
+        var startX = 0f
+
+        val listView002 = findViewById<LinearLayout>(R.id.testListView002)
+        listView002.setOnTouchListener { v, event ->
+
+//            Log.d("Kimse", "setOnTouchListener : ${event.action}")
+
+
+            when (event.action) {
+
+                MotionEvent.ACTION_DOWN -> {
+                    startX = event.x
+                }
+
+                MotionEvent.ACTION_MOVE -> {
+                    val movedX: Float = event.x - startX
+
+                    if (v.x + movedX > 420) {
+                        v.x = 420f
+                        headerView002.x = 420f
+                    } else {
+                        v.x = v.x + movedX
+                        headerView002.x = headerView002.x + movedX
+                    }
+                }
+            }
+
+            return@setOnTouchListener true
         }
-        adapter.notifyDataSetChanged()
+
+
+        val listView001 = findViewById<LinearLayout>(R.id.testListView001)
+
+        val inflater = LayoutInflater.from(this)
+
+
+        for (i in 0..100) {
+            val itemView = inflater.inflate(R.layout.listitem, null)
+            itemView.findViewById<TextView>(R.id.itemTitle).text = "ITEM = ${i}"
+            listView001.addView(itemView)
+        }
+
+        for (i in 0..100) {
+            val itemView = inflater.inflate(R.layout.listitem, null)
+            itemView.findViewById<TextView>(R.id.itemTitle).text = "HORIZONTAL SCROLL ITEM = ${i}"
+            listView002.addView(itemView)
+        }
 
     }
+
+
 }
